ca30da4794ccb96ff7215531307dcef7e799d1e7
Colin Brayton <gringolalia@gmail.com>
2020-07-29 10:38:29 -0300

    deploy

---
+++ precache-manifest.246d77b49969468e834bfd3c2c3d9118.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "207786c8d8715956d05a",
+    "url": "/multiuse-chanteuse/css/about.00506d4d.css"
+  },
+  {
+    "revision": "68133cbd5a4255a05204",
+    "url": "/multiuse-chanteuse/css/app.5612d6b6.css"
+  },
+  {
+    "revision": "954421b4110eb5860401",
+    "url": "/multiuse-chanteuse/css/chunk-vendors.1b2e7df7.css"
+  },
+  {
+    "revision": "040faae9f93b143eda18261d5d87e989",
+    "url": "/multiuse-chanteuse/fonts/news-cycle-latin-400.040faae9.woff2"
+  },
+  {
+    "revision": "22544f4afc7d8af32e46a72d7351e7a0",
+    "url": "/multiuse-chanteuse/fonts/news-cycle-latin-400.22544f4a.woff"
+  },
+  {
+    "revision": "7cc90a67675ad267be8c511c2a63eae1",
+    "url": "/multiuse-chanteuse/fonts/news-cycle-latin-700.7cc90a67.woff2"
+  },
+  {
+    "revision": "e692318c15c39c23513a237c42592b79",
+    "url": "/multiuse-chanteuse/fonts/news-cycle-latin-700.e692318c.woff"
+  },
+  {
+    "revision": "13d5b92ab2f3b8a5b2675fbb667e7b52",
+    "url": "/multiuse-chanteuse/img/midtowwnfrown.13d5b92a.jpg"
+  },
+  {
+    "revision": "ce029b764e9f052e9a4a1c7fc28bbd2e",
+    "url": "/multiuse-chanteuse/img/thewhofflag.ce029b76.jpg"
+  },
+  {
+    "revision": "279dcec949579055f7238a2bf5eb00a4",
+    "url": "/multiuse-chanteuse/img/vintage-test-two.279dcec9.jpeg"
+  },
+  {
+    "revision": "a6acee179778aa0a7f9fe0dcec52ee6a",
+    "url": "/multiuse-chanteuse/index.html"
+  },
+  {
+    "revision": "207786c8d8715956d05a",
+    "url": "/multiuse-chanteuse/js/about.c92c6f62.js"
+  },
+  {
+    "revision": "68133cbd5a4255a05204",
+    "url": "/multiuse-chanteuse/js/app.f59e9742.js"
+  },
+  {
+    "revision": "954421b4110eb5860401",
+    "url": "/multiuse-chanteuse/js/chunk-vendors.36d02ecd.js"
+  },
+  {
+    "revision": "0fa05f879a2f0cc11495fbed62f7bcd2",
+    "url": "/multiuse-chanteuse/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/multiuse-chanteuse/robots.txt"
+  }
+]);
